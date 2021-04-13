3f4f4f5e1859c9b019f19506f2f99e0ed5dd539b
Dan Hill <dhill@promoted.ai>
2021-04-13 13:27:46 -1000

    feat: export schema types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,26 @@
 export * from './client';
 export * from './experiment';
+
+// Updated manually.
+export type { Properties, Timing, UserInfo } from './types/common';
+export type {
+  BlenderRule,
+  BlenderRuleType,
+  BlenderRuleTypeMap,
+  BlenderRuleTypeString,
+  DeliveryConfig,
+  Insertion,
+  Request,
+  Response,
+  UseCase,
+  UseCaseString,
+  UseCaseMap,
+} from './types/delivery';
+export type {
+  CohortArm,
+  CohortArmMap,
+  CohortArmString,
+  CohortMembership,
+  LogRequest,
+  LogResponse,
+} from './types/event';
