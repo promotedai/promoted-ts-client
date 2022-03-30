5bf876029ef68d081490142b67578a2fed00df4c
Scott McMaster <scott@promoted.ai>
2022-03-30 08:44:55 -0700

    feat: Bump version for Browser.referrer field\rTESTING=unit (#61)

--- src/client.ts
+++ src/client.ts
@@ -14,7 +14,7 @@ import { ExecutionServer } from './execution-server';
 
 // Version number that semver will generate for the package.
 // Must be manually maintained.
-export const SERVER_VERSION = 'ts.8.1.0';
+export const SERVER_VERSION = 'ts.8.2.0';
 
 /**
  * Design-wise
