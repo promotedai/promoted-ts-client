9431c740519da3acef368186db4038aad74f2d7d
Dan Hill <dhill@promoted.ai>
2022-08-05 15:49:36 -0700

    docs: update comments

--- src/delivery-request.ts
+++ src/delivery-request.ts
@@ -11,26 +11,28 @@ export interface DeliveryRequest {
   request: Request;
 
   /**
-   * Clients should send a subset of all request insertions to Promoted on
+   * Clients can send a subset of all request insertions to Promoted on
    * `request.insertion`.  The `insertionStart` specifies the start index of
    * the array `request.insertion` in the list of all request insertions.
    *
-   * `request.paging.offset` should be set to the zero-based position in all request insertions
-   * (not the relative position in `request.insertion`s).
+   * `request.paging.offset` should be set to the zero-based position in all
+   * request insertions (not the relative position in `request.insertion`s).
    *
    * Examples:
    * a. If there are 10 items and all 10 items are in `request.insertion`, then
    *    insertionStart=0.
    * b. If there are 10,000 items and the first 500 items are on `request.insertion`,
    *    then insertionStart=0.
-   * c. If there are 10,000 items and we want to send items [500,1000) on `request.insertion`,
-   *    then insertionStart=500.
-   * c. If there are 10,000 items and we want to send items the last page [9500,10000) on `request.insertion`,
-   *    then insertionStart=9500.
+   * c. If there are 10,000 items and we want to send items [500,1000) on
+   *    `request.insertion`, then insertionStart=500.
+   * c. If there are 10,000 items and we want to send the last page [9500,10000)
+   *    on `request.insertion`, then insertionStart=9500.
    *
-   * This field is required because it will likely be a source of bugs.
+   * This field is required because an incorrect value could result in a bad bug.
+   * If you only send the first X request insertions, then insertionStart=0.
    *
-   * TODO - create a visualization and link to it.
+   * Follow this link for more details.
+   * https://docs.promoted.ai/docs/ranking-requests#sending-even-more-request-insertions
    */
   insertionStart: number;
 
