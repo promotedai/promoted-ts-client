8075f143bd376f77a5fe8694906c2015a46cb0d1
Dan Hill <dhill@promoted.ai>
2023-03-15 13:30:42 -0700

    fix: bump version

--- src/client.ts
+++ src/client.ts
@@ -14,7 +14,7 @@ import { Validator, validateResponse } from './validator';
 
 // Version number that semver will generate for the package.
 // Must be manually maintained.
-export const SERVER_VERSION = 'ts.11.0.2';
+export const SERVER_VERSION = 'ts.11.0.3';
 
 /**
  * Design-wise
